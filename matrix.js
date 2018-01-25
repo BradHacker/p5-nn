84c914821f414c72fc754e018c2508e2d70caa89
BradHacker <harkerbd@gmail.com>
2018-01-25 17:39:44 -0500

    Update Matrix Library.

--- matrix.js
+++ matrix.js
@@ -4,19 +4,65 @@ class Matrix {
     this.cols = cols;
     this.matrix = new Array(rows);
 
-    for (var i = 0; i < this.rows; i++) {
+    for (let i = 0; i < this.rows; i++) {
       this.matrix[i] = new Array(cols);
-      for (var j = 0; j < this.cols; j++) {
+      for (let j = 0; j < this.cols; j++) {
         this.matrix[i][j] = 0;
       }
     }
   }
 
   randomize() {
-    for (var i = 0; i < this.rows; i++) {
-      for (var j = 0; j < this.cols; j++) {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
         this.matrix[i][j] = Math.floor(Math.random()*10);
       }
     }
+  }
+
+  add(other) {
+    // Are we trying to add a Matrix?
+    if (other instanceof Matrix) {
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          this.matrix[i][j] += other.matrix[i][j];
+        }
+      }
+    // Or just a single scalar value?
+    } else {
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          this.matrix[i][j] += other;
+        }
+      }
+    }
+  }
+
+  multiply(other) {
+    // Are we trying to multiply a Matrix?
+    if (other instanceof Matrix) {
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          this.matrix[i][j] *= other.matrix[i][j];
+        }
+      }
+      // Or just a single scalar value?
+    } else {
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          this.matrix[i][j] *= other;
+        }
+      }
+    }
+  }
+
+  transpose() {
+    let result = new Matrix(this.cols, this.rows);
+    for (let i = 0; i < result.rows; i++) {
+      for (let j = 0; j < result.cols; j++) {
+        result.matrix[i][j] = this.matrix[j][i];
+      }
+    }
+    return result;
   }
 }
